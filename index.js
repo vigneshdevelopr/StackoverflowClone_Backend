2b9033a425ce939bd5b9c373531a0f8252603f4f
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-09-02 15:58:02 +0530

    Add Cron to Ping auto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { loginRouter } from "./Routes/login.js";
 import { signupRouter } from "./Routes/signup.js";
 import { doubtscontent } from "./Routes/doubtscontent.js";
 import { isSignedIn } from "./Auth/auth.js";
-
+import cron from 'node-cron'
 const app = express();
 
 const PORT = process.env.port;
@@ -16,14 +16,22 @@ app.use(express.json());
 app.use(cors());
 app.use("/user", signupRouter);
 app.use("/user", loginRouter);
+app.get('/',(req,res)=>{
+  return res.status(200).json({message: 'Stackoverflow server working'})
+})
 app.use("/",isSignedIn, doubtscontent);
 createConnection();
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+
+cron.schedule('* * * * *', () => {
+  console.log('running a task every minute');
 });
 //================================================================
 
-app.get("/", (req, res) => {
-  res.status(200).send("working");
-  console.log("working");
+app.get("/home", (req, res) => {
+ return res.status(200).send("working");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
